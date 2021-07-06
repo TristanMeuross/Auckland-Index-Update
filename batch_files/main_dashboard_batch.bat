2ac597b2d4a0e49506b2df9513b8a8b29fd239f7
TristanMeuross <61038321+TristanMeuross@users.noreply.github.com>
2021-07-06 15:15:23 +1200

    Setting up batch files
    Created batch files for covid dashboard and main auckland index scripts

---
+++ batch_files/main_dashboard_batch.bat
@@ -0,0 +1,2 @@
+call "C:\Users\meurost\AppData\Local\Continuum\anaconda3\Scripts\activate.bat" auckland-index & cd "C:\Users\meurost\Auckland-Index-Update" & python "auckland_index_update_main.py"
+IF %ERRORLEVEL% NEQ 0 PAUSE
