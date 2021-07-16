8bc486e6d2070939d23609e844661ef1fc363a4f
TristanMeuross <61038321+TristanMeuross@users.noreply.github.com>
2021-07-16 12:57:52 +1200

    Updated batch files for pythonpath
    Added python path for open-stats-api path for module uploading

--- batch_files/covid_dashboard_batch.bat
+++ batch_files/covid_dashboard_batch.bat
@@ -1,2 +1,5 @@
+setlocal
+set PYTHONPATH=%USERPROFILE%\open-data-api
 call "C:\Users\meurost\AppData\Local\Continuum\anaconda3\Scripts\activate.bat" auckland-index & cd "C:\Users\meurost\Auckland-Index-Update" & python "auckland_index_update_covid.py"
 IF %ERRORLEVEL% NEQ 0 PAUSE
+endlocal
